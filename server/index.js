27d950cd6b3a904a138caa4ed3be37adcca5aa9a
Raz <razik.gvili@gmail.com>
2020-06-10 18:25:18 +0300

    server fix

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ pgClient.on('error', () => { console.log('Lost PG connection')})
 
 pgClient
 .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+.then(result => console.log("table created"))
 .catch(err => console.log(err))
 
 
